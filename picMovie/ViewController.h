35ca15da1af43868dfe866249c59867056b05f0b
chenmingzheng <mingzheng@cmz-pro.local>
2015-09-07 10:57:31 +0800

    Initial Commit

---
+++ picMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  picMovie
+//
+//  Created by 名正 on 15/9/7.
+//  Copyright (c) 2015年 mingzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ picMovie/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  picMovie
+//
+//  Created by 名正 on 15/9/7.
+//  Copyright (c) 2015年 mingzheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
